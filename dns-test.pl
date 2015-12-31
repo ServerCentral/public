1d52bf52f29b915f7b6a772656b648b6e1639e52
pdoroff <phil21@phil21.net>
2015-12-31 03:10:27 -0600

    SIGINT support and other assorted novelty

--- dns-test.pl
+++ dns-test.pl
@@ -4,6 +4,10 @@ use v5.10;
 $| =0;
 
 my $sleep = 0.25;
+my $query = 'app.goclio.com';
+
+my $server = $ARGV[0] || '64.202.97.1';
+my $fqdn = $ARGV[1] || 'servercentral.com';
 
 use Time::Piece;
 use Time::HiRes;
@@ -12,14 +16,27 @@ use POSIX qw/strftime/;
 my $err = 0;
 my $ok = 0;
 
-$SIG{INT} = sub { say "Successful Queries: $ok Failed: $err"; exit 0; };
-say "Starting up with $sleep sleep between queries";
+my $last_sigint = 0;
+
+$SIG{INT} = sub {
+  my $ts = time();
+  if (time - $last_sigint < 2) {
+    say "\nExiting... $server $ok OK, $err FAIL";
+    exit 0;
+  } else {
+    say "\n$server successful Queries: $ok Failed: $err";
+    say "Ctrl-C again to exit";
+  }
+  $last_sigint = time();
+};
+
+say "Starting up with $sleep sleep between queries to server $server";
 while (1) {
   my $ip = '';
   my $t = Time::HiRes::time;
   my $s = sprintf "%06.3f", $t-int($t/60)*60;
   my $ts = strftime "%H:%M:$s ", localtime $t;
-  my @cmd = `host -s -t A -W 1 -t A -R 0 app.goclio.com 64.202.97.1`;
+  my @cmd = `host -s -t A -W 1 -t A -R 0 $fqdn $server`;
   for (@cmd) {
     if (/has address ([0-9\.]+)$/) {
       $ok++;
